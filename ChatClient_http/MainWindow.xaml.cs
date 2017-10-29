193e95b0bc487f692372c4bbdb56e4792a8920ae
Alex Grigurko <alexgrigurko@ukr.net>
2017-10-29 22:12:58 +0200

    Добавьте файлы проекта.

---
+++ ChatClient_http/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace HttpChatClientD
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        HttpClient client;
+        Thread thread;
+        string ip;
+
+        public MainWindow()
+        {
+            client = new HttpClient();
+            thread = new Thread(StartListen);
+            InitializeComponent();                      
+        }
+
+        private async void StartListen()
+        {
+            while(true)
+            {
+                try
+                {
+                    string response = await client.GetStringAsync(ip + "?check=true");
+                    string[] param = response.Split('=');
+                    if (param[0] == "message")
+                        Dispatcher.Invoke(() => chatList.Items.Add("Server: " + param[1]));                     
+                }
+                catch(Exception e)
+                {
+                    Dispatcher.Invoke(() => chatList.Items.Add("Cannot connect!"));
+                }
+                Thread.Sleep(500);
+            }
+        }
+
+        private async void SendClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var response = await client.PostAsync(ip, new StringContent("message=" + inputTB.Text));
+                string content = await response.Content.ReadAsStringAsync();
+                if(content == "received")
+                    chatList.Items.Add("You: " + inputTB.Text);
+            }
+            catch
+            {
+                chatList.Items.Add("Cannot connect!");
+            }
+        }
+
+        private void ConnectClick(object sender, RoutedEventArgs e)
+        {
+            ip = "http://" + ipTB.Text;
+            if (thread.IsAlive == false)
+                thread.Start();
+            chatList.Items.Add("You've connected to server!");
+        }
+    }
+}
---
+++ ChatServer_http/HttpServer.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ChatServet_http
+{
+    class HttpServer
+    {
+        HttpListener listener;
+        string lastMessage = "";
+        Dictionary <Cookie, bool> clients;
+
+        public HttpServer(string uri)
+        {
+            clients = new Dictionary<Cookie, bool>();
+            listener = new HttpListener();
+            listener.Prefixes.Add(uri);
+        }
+
+        public void Start()
+        {
+            listener.Start();
+            Console.WriteLine("Listening...");
+
+            while (true)
+            {
+                try
+                {
+                    HttpListenerContext context = listener.GetContext();
+                    Receiver(context);
+                }
+                catch(Exception e)
+                {
+
+                }
+            }
+
+        }
+
+        private void Receiver(HttpListenerContext context)
+        {
+            Dictionary<string, string> param = ParseRequest(context.Request);
+            if (param.ContainsKey("message"))
+            {
+                lastMessage = param["message"];
+                for(int i = 0; i< clients.Count; i++)
+                {
+                    clients[clients.Keys.ElementAt(i)] = true;
+                }
+                SendData(context, "received");
+            }
+            else if(param.ContainsKey("check"))
+            {
+                Cookie cookie = context.Request.Cookies["ID"];
+                if (cookie == null)
+                {
+                    Cookie newCookie = new Cookie("ID", DateTime.Now.ToString());
+                    context.Response.AppendCookie(newCookie);
+                    clients.Add(newCookie, false);
+                    SendData(context, "nomessage=true");                  
+                }
+                else
+                {
+                    if (clients[cookie] == true)
+                    {
+                        SendData(context, "message=" + lastMessage);
+                        clients[cookie] = false;
+                    }
+                    else
+                    {
+                        SendData(context, "nomessage=true");
+                    }
+                }
+            }
+        }
+
+        private void SendData(HttpListenerContext context, string data)
+        {
+            context.Response.ContentType = "text/plain";
+            context.Response.AddHeader("Access-Control-Allow-Origin", "*");
+            context.Response.AddHeader("Access-Control-Allow-Credentials", "true");
+            context.Response.AddHeader("Access-Control-Expose-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization"); 
+            context.Response.ContentLength64 = data.Length;
+            context.Response.OutputStream.Write(Encoding.ASCII.GetBytes(data), 0, data.Length);
+            context.Response.OutputStream.Close();
+        }
+
+        private Dictionary<string,string> ParseRequest(HttpListenerRequest request)
+        {
+            Dictionary<string, string> param = new Dictionary<string, string>();
+
+            if (request.HttpMethod == "POST")
+            {
+                string body = new StreamReader(request.InputStream).ReadToEnd();
+                string[] temp = body.Split('=', '&');
+                for (int i = 0; i < temp.Length; i+=2)
+                {
+                    param.Add(temp[i], temp[i + 1]);
+                }              
+            }
+            else
+            {
+                string[] temp = request.QueryString.AllKeys;
+                foreach(string key in temp)
+                {
+                    param.Add(key, request.QueryString[key]);
+                }
+            }
+
+            return param;
+        }
+    }
+}
